c4a6a17bd9cb85ba449a1fdbbda010316bc0c259
lironscallealta <lironscallealta@gmail.com>
2026-05-25 16:34:30 -0400

    init repository

---
+++ src/test/java/cl/duoc/accounting_manager/AccountingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.accounting_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
