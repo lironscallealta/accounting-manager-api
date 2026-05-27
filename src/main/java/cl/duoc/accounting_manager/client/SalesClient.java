dff7978c5e99d9c8bdc46a48a1cf42ba47a6dcb0
lironscallealta <lironscallealta@gmail.com>
2026-05-26 23:04:25 -0400

    feat: add WebClient configuration for sales and invoices services

--- src/main/java/cl/duoc/accounting_manager/client/SalesClient.java
+++ src/main/java/cl/duoc/accounting_manager/client/SalesClient.java
@@ -1,70 +1,115 @@
-package cl.duoc.accounting_manager.client;
-
-import java.util.List;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import cl.duoc.accounting_manager.dto.request.sales.SaleCreationRequest;
-import cl.duoc.accounting_manager.dto.request.sales.SaleUpdateRequest;
-import cl.duoc.accounting_manager.dto.response.sales.SaleResponse;
-import cl.duoc.accounting_manager.dto.response.sales.SaleStatusResponse;
-import lombok.RequiredArgsConstructor;
-
-@Component
-@RequiredArgsConstructor
-public class SalesClient {
-
-    private final WebClient webClientSales;
-
-    public SaleResponse findSale(Long id) {
-        return webClientSales.get()
-                .uri("/{id}", id)
-                .retrieve()
-                .bodyToMono(SaleResponse.class)
-                .block();
-    }
-
-    public List<SaleResponse> findAllSales() {
-        return webClientSales.get()
-                .retrieve()
-                .bodyToFlux(SaleResponse.class)
-                .collectList()
-                .block();
-    }
-
-    public SaleResponse saveSale(SaleCreationRequest req) {
-        return webClientSales.post()
-                .bodyValue(req)
-                .retrieve()
-                .bodyToMono(SaleResponse.class)
-                .block();
-    }
-
-    public SaleResponse replaceSale(Long id, SaleUpdateRequest req) {
-        return webClientSales.put()
-                .uri("/{id}", id)
-                .bodyValue(req)
-                .retrieve()
-                .bodyToMono(SaleResponse.class)
-                .block();
-    }
-
-    public void deleteSale(Long id) {
-        webClientSales.delete()
-                .uri("/{id}", id)
-                .retrieve()
-                .toBodilessEntity()
-                .block();
-    }
-
-    public List<SaleStatusResponse> findAllSaleStatus() {
-        return webClientSales.get()
-                .uri("/status")
-                .retrieve()
-                .bodyToFlux(SaleStatusResponse.class)
-                .collectList()
-                .block();
-    }
-}
-
+package cl.duoc.accounting_manager.client;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import org.springframework.web.reactive.function.client.WebClient;
+
+import cl.duoc.accounting_manager.dto.request.sales.SaleCreationRequest;
+
+import cl.duoc.accounting_manager.dto.request.sales.SaleUpdateRequest;
+
+import cl.duoc.accounting_manager.dto.response.sales.SaleResponse;
+
+import cl.duoc.accounting_manager.dto.response.sales.SaleStatusResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+
+@RequiredArgsConstructor
+
+public class SalesClient {
+
+    private final WebClient webClientSales;
+
+    public SaleResponse findSale(Long id) {
+
+        return webClientSales.get()
+
+                .uri("/{id}", id)
+
+                .retrieve()
+
+                .bodyToMono(SaleResponse.class)
+
+                .block();
+
+    }
+
+    public List<SaleResponse> findAllSales() {
+
+        return webClientSales.get()
+
+                .retrieve()
+
+                .bodyToFlux(SaleResponse.class)
+
+                .collectList()
+
+                .block();
+
+    }
+
+    public SaleResponse saveSale(SaleCreationRequest req) {
+
+        return webClientSales.post()
+
+                .bodyValue(req)
+
+                .retrieve()
+
+                .bodyToMono(SaleResponse.class)
+
+                .block();
+
+    }
+
+    public SaleResponse replaceSale(Long id, SaleUpdateRequest req) {
+
+        return webClientSales.put()
+
+                .uri("/{id}", id)
+
+                .bodyValue(req)
+
+                .retrieve()
+
+                .bodyToMono(SaleResponse.class)
+
+                .block();
+
+    }
+
+    public void deleteSale(Long id) {
+
+        webClientSales.delete()
+
+                .uri("/{id}", id)
+
+                .retrieve()
+
+                .toBodilessEntity()
+
+                .block();
+
+    }
+
+    public List<SaleStatusResponse> findAllSaleStatus() {
+
+        return webClientSales.get()
+
+                .uri("/status")
+
+                .retrieve()
+
+                .bodyToFlux(SaleStatusResponse.class)
+
+                .collectList()
+
+                .block();
+
+    }
+
+}
--- src/main/java/cl/duoc/accounting_manager/config/WebClientConfig.java
+++ src/main/java/cl/duoc/accounting_manager/config/WebClientConfig.java
@@ -1,5 +1,24 @@
 package cl.duoc.accounting_manager.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class WebClientConfig {
 
+    @Bean
+    public WebClient webClientSales(@Value("${services.sales.base-url}") String baseUrl) {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
+
+    @Bean
+    public WebClient webClientInvoice(@Value("${services.invoices.base-url}") String baseUrl) {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
 }
