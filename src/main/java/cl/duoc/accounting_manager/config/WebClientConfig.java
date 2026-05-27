ae7f4938eae3178d641a6a9f529d8b23f8f5e005
lironscallealta <lironscallealta@gmail.com>
2026-05-26 23:12:10 -0400

    refactor: update WebClient configuration

--- src/main/java/cl/duoc/accounting_manager/config/WebClientConfig.java
+++ src/main/java/cl/duoc/accounting_manager/config/WebClientConfig.java
@@ -1,24 +1,25 @@
 package cl.duoc.accounting_manager.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Configuration
+@Configuration // Esta clase contiene configuración del contexto (beans)
 public class WebClientConfig {
 
-    @Bean
-    public WebClient webClientSales(@Value("${services.sales.base-url}") String baseUrl) {
-        return WebClient.builder()
-                .baseUrl(baseUrl)
-                .build();
+    @Bean // Ejecuta este método y guarda su resultado como un objeto administrado. La deja disponible para inyección (@Autowired o constructor)
+    public WebClient webClientSales() {
+        return WebClient.builder() // Crea un builder para configurar el cliente HTTP
+                .baseUrl("http://localhost:8082/api/v1/sales") // Define la URL base del microservicio de ventas
+                .defaultHeader("Content-Type", "application/json") // Agrega un header HTTP por defecto a TODAS las solicitudes
+                .build(); // Construye el objeto final WebClient
     }
 
-    @Bean
-    public WebClient webClientInvoice(@Value("${services.invoices.base-url}") String baseUrl) {
-        return WebClient.builder()
-                .baseUrl(baseUrl)
-                .build();
+    @Bean // Ejecuta este método y guarda su resultado como un objeto administrado. La deja disponible para inyección (@Autowired o constructor)
+    public WebClient webClientInvoice() {
+        return WebClient.builder() // Crea un builder para configurar el cliente HTTP
+                .baseUrl("http://localhost:8087/api/v1/invoices") // Define la URL base del microservicio de facturas
+                .defaultHeader("Content-Type", "application/json") // Agrega un header HTTP por defecto a TODAS las solicitudes
+                .build(); // Construye el objeto final WebClient
     }
 }
